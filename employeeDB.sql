926659a6d9dd72fceb1d3162fb660e16ab1616d3
Brandon Petersen <89234922+bdpetersen13@users.noreply.github.com>
2023-12-06 13:47:42 -0500

    Update employeeDB.sql
    Continuing to build the employeeDB by adding primary keys and foreign key constraints, and adding index on each of the tables

--- employeeDB.sql
+++ employeeDB.sql
@@ -118,3 +118,94 @@ ALTER COLUMN date_of_hire SET NOT NULL;
 ALTER TABLE employees
 ALTER COLUMN contact_number SET DATA TYPE VARCHAR,
 ADD COLUMN email_address VARCHAR(100); 
+
+-- Define data type for address
+ALTER TABLE employees
+ALTER COLUMN address SET DATA TYPE VARCHAR;
+
+-- Define data type for emergency contact information
+ALTER TABLE employees
+ALTER COLUMN emergency_contact_number SET DATA TYPE VARCHAR,
+ALTER COLUMN emergency_contact_name SET DATA TYPE VARCHAR;
+
+-- Add foreign key constraint to the department column
+ALTER TABLE employees
+ADD CONSTRAINT fk_employees_department
+FOREIGN KEY (department_id) REFERENCES departments (department_id);
+
+-- Add foreign key constraint to the position/role column
+ALTER TABLE employees
+ADD CONSTRAINT fk_employees_roles
+FOREIGN KEY (role_id) REFERENCES roles (role_id);
+
+-- Index on the employees table
+-- Index on the primary key
+CREATE INDEX idx_employees_primary_key
+ON employees (employee_id);
+
+-- Index on the foreign key columns (department_id and role_id)
+CREATE INDEX idx_employees_department_id
+ON employees (department_id);
+
+CREATE INDEX idx_employees_role_id
+ON employees (role_id);
+
+-- Add indexes for contact information if frequently queried
+CREATE INDEX idx_employees_phone_number
+ON employees (contact_number);
+
+CREATE INDEX idx_employees_email
+ON employees (email_address);
+
+-- Index on emergency contact information if frequently queried
+CREATE INDEX idx_employees_emergency_contact_name
+ON employees (emergency_contact_name);
+
+CREATE INDEX idx_employees_emergency_contact_number
+ON employees (emergency_contact_number);
+
+-- Index on the departments table
+-- Index on the primary key
+CREATE INDEX idx_departments_primary_key
+ON departments (department_id);
+
+-- Index on department names
+CREATE INDEX idx_departments_department_name
+ON departments (department_name);
+
+-- Index on the roles table
+-- Index on the primary key
+CREATE INDEX idx_roles_primary_key
+ON roles (role_id);
+
+-- Index on the roles name
+CREATE INDEX idx_roles_role_name
+ON roles (role_name);
+
+-- Index on the performance reviews table
+-- Index on the primary key
+CREATE INDEX idx_performance_reviews_primary_key
+ON performance_reviews (review_id);
+
+-- Index on the foreign key column (employee_id)
+CREATE INDEX idx_performance_reviews_employee_id
+ON performance_reviews (employee_id);
+
+-- Index on the salary table
+-- Index on the primary key
+CREATE INDEX idx_salaries_primary_key
+ON salaries (salary_id);
+
+-- Index on the foreign key column (employee_id)
+CREATE INDEX idx_salaries_employee_id
+ON salaries (employee_id);
+
+-- Index on the training table
+-- Index on the primary key
+CREATE INDEX idx_training_primary_key
+ON training (training_id);
+
+-- Index on the foreign key column (employee_id)
+CREATE INDEX idx_training_employee_id
+ON training (employee_id);
+
